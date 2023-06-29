061bce9900feea67866b73298c96c07b84f1aff3
a2359314426 <50039036+a2359314426@users.noreply.github.com>
2023-06-29 14:48:32 +0800

    Create ThreadPool.cpp

---
+++ ThreadPool.cpp
@@ -0,0 +1,33 @@
+#include "Algorithms/DecodeH264/ThreadPool.h"
+#include "Utils/Logger.h"
+ThreadPool::ThreadPool(size_t numThreads)
+{
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers.emplace_back([this] {
+            for (;;) {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                    if (this->stop && this->tasks.empty()) {
+                        return;
+                    }
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                }
+                task();
+            }
+        });
+        }
+}
+ThreadPool::~ThreadPool ()
+{
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
